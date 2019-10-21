cb9eb0be3f8995e29f207944552670f791b48277
Wilma <kima_92@fastmail.com>
2019-10-21 18:22:52 -0400

    Implemented Log In and Sign Up functionality

---
+++ Reciept Tracker/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  ReceiptTracker
+//
+//  Created by macbook on 10/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
